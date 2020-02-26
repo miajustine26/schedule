e5c7762fca963ff71d3c9a9660072acdb4d39eb9
miajustine26 <mjgeneroso267@gmail.com>
2020-02-26 16:23:08 -0500

    Add time to dictionary

---
+++ dictionary.py
@@ -0,0 +1,345 @@
+posts = {
+    "Monday" : [
+        {
+            "class" : "History",
+            "room" : "Right Lobby",
+            "teacher" : "David",
+            "time" : "8:30",
+            "css" : "his"
+        },
+        {
+            "class" : "Physics",
+            "room" : "1A",
+            "teacher" : "John",
+            "time" : "9:15",
+            "css" : "phy"
+        },
+        {
+            "class" : "Calculus",
+            "room" : "Lab",
+            "teacher" : "John",
+            "time" : "10:00",
+            "css" : "cal"
+        },
+        {
+            "class" : "SNACK",
+            "time" : "10:45"
+        },
+        {
+            "class" : "Latin",
+            "room" : "3A",
+            "teacher" : "Bruce",
+            "time" : "10:55",
+            "css" : "lat"
+        },
+        {
+            "class" : "Chinese",
+            "room" : "3A",
+            "teacher" : "Joy",
+            "time" : "11:40",
+            "css" : "chi"
+        },
+        {
+            "class" : "Advisory",
+            "teacher" : "Jon",
+            "room" : "",
+            "time" : "12:25",
+            "css" : "adv"
+
+        },
+        {
+            "class" : "Art",
+            "room" : "Art Room",
+            "teacher" : "Kevin",
+            "time" : "1:10",
+            "css" : "art"
+        },
+        {
+            "class" : "English",
+            "room" : "3A",
+            "teacher" : "Amelia",
+            "time" : "1:55",
+            "css" : "eng"
+        },
+        {
+            "class" : "Greek",
+            "room" : "Not 2B",
+            "teacher" : "Marya",
+            "time" : "2:40",
+            "css" : "gre"
+        }
+    ],
+    "Tuesday" : [
+        {
+            "class" : "Latin",
+            "room" : "3A",
+            "teacher" : "Bruce",
+            "time" : "8:30",
+            "css" : "lat"
+        },
+        {
+            "class" : "Italian",
+            "room" : "Alcove",
+            "teacher" : "Federica",
+            "time" : "9:15",
+            "css" : "ita"
+        },
+        {
+            "class" : "Chinese",
+            "room" : "3A",
+            "teacher" : "Joy",
+            "time" : "10:00",
+            "css" : "chi"
+        },
+        {
+            "class" : "SNACK",
+            "time" : "10:45"
+        },
+        {
+            "class" : "Computer Science",
+            "room" : "2F",
+            "teacher" : "Julio",
+            "time" : "10:55",
+            "css" : "cs"
+        },
+        {
+            "class" : "History",
+            "room" : "Right Lobby",
+            "teacher" : "David",
+            "time" : "11:40",
+            "css" : "his"
+        },
+        {
+            "class" : "Theater",
+            "room" : "Left Lobby",
+            "teacher" : "Jesse",
+            "time" : "12:25",
+            "css" : "the"
+        },
+        {
+            "class" : "Free",
+            "room" : "Alcove",
+            "teacher": "",
+            "time" : "1:10",
+            "css" : "free"
+        },
+        {
+            "class" : "Physics",
+            "room" : "1A",
+            "teacher" : "John",
+            "time" : "1:55",
+            "css" : "phy"
+        },
+        {
+            "class" : "Greek",
+            "room" : "Not 2B",
+            "teacher" : "Marya",
+            "time" : "2:40",
+            "css" : "gre"
+        }
+    ],
+    "Wednesday" : [
+        {
+            "class" : "Physics",
+            "room" : "1A",
+            "teacher" : "John",
+            "time" : "8:30",
+            "css" : "phy"
+        },
+        {
+            "class" : "Calculus",
+            "room" : "Lab",
+            "teacher" : "John",
+            "time" : "9:15",
+            "css" : "cal"
+        },
+        {
+            "class" : "English",
+            "room" : "3A",
+            "teacher" : "Amelia",
+            "time" : "10:00",
+            "css" : "eng"
+        },
+        {
+            "class" : "SNACK",
+            "time" : "10:45"
+        },
+        {
+            "class" : "Modern Dance",
+            "room" : "Dance Room",
+            "teacher" : "Dana, Jamal",
+            "time" : "10:55",
+            "css" : "dan"
+        },
+        {
+            "class" : "Modern Dance",
+            "room" : "Dance Room",
+            "teacher" : "Dana, Jamal",
+            "time" : "11:40",
+            "css" : "dan"
+        },
+        {
+            "class" : "Advisory/Cheer Team",
+            "teacher" : "Dana, Amelia",
+            "room" : "",
+            "time" : "12:25",
+            "css" : "adv"
+        },
+        {
+            "class" : "Study",
+            "room" : "Alcove",
+            "teacher" : "",
+            "time" : "1:10",
+            "css" : "free"
+        },
+        {
+            "class" : "Greek",
+            "room" : "3A",
+            "teacher" : "Marya",
+            "time" : "1:55",
+            "css" : "gre"
+        },
+        {
+            "class" : "Free",
+            "room" : "Alcove",
+            "teacher" : "",
+            "time" : "2:40",
+            "css" : "free"
+        }
+    ],
+    "Thursday" : [
+        {
+            "class" : "Chinese",
+            "room" : "3A",
+            "teacher" : "Joy",
+            "time": "8:30",
+            "css" : "chi"
+        },
+        {
+            "class" : "Computer Science",
+            "room" : "2F",
+            "teacher" : "Julio",
+            "time" : "9:15",
+            "css" : "cs"
+        },
+        {
+            "class" : "Calculus",
+            "room" : "Lab",
+            "teacher" : "John",
+            "time" : "10:00",
+            "css" : "cal"
+        },
+        {
+            "class" : "SNACK",
+            "time" : "10:45"
+        },
+        {
+            "class" : "Greek",
+            "room" : "Not 2B",
+            "teacher" : "Marya",
+            "time" : "10:55",
+            "css" : "gre"
+        },
+        {
+            "class" : "Latin",
+            "room" : "3A",
+            "teacher" : "Bruce",
+            "time" : "11:40",
+            "css" : "lat"
+        },
+        {
+            "class" : "LUNCH",
+            "time" : "12:25"
+        },
+        {
+            "class" : "African Dance",
+            "room" : "Dance Room",
+            "teacher" : "Jamal",
+            "time" : "1:10",
+            "css" : "dan"
+        },
+        {
+            "class" : "English",
+            "room" : "3A",
+            "teacher" : "Amelia",
+            "time" : "1:55",
+            "css" : "eng"
+        },
+        {
+            "class" : "History",
+            "room" : "Right Lobby",
+            "teacher" : "David",
+            "time" : "2:40",
+            "css" : "his"
+        }
+    ],
+    "Friday" : [
+        {
+            "class" : "English",
+            "room" : "3A",
+            "teacher" : "Amelia",
+            "time" : "8:30",
+            "css" : "eng"
+        },
+        {
+            "class" : "Art",
+            "room" : "Art Room",
+            "teacher" : "Kevin",
+            "time" : "9:15",
+            "css" : "art"
+        },
+        {
+            "class" : "Computer Science",
+            "room" : "2F",
+            "teacher" : "Julio",
+            "time" : "10:00",
+            "css" : "cs"
+        },
+        {
+            "class" : "SNACK",
+            "time" : "10:45"
+        },
+        {
+            "class" : "Latin",
+            "room" : "3A",
+            "teacher" : "Bruce",
+            "time" : "10:55",
+            "css" : "lat"
+        },
+        {
+            "class" : "History",
+            "room" : "Right Lobby",
+            "teacher" : "David",
+            "time" : "11:40",
+            "css" : "his"
+        },
+        {
+            "class" : "Choreo Lab",
+            "room" : "Dance Room",
+            "teacher" : "Dana",
+            "time" : "12:25",
+            "css" : "cho"
+        },
+        {
+            "class" : "Calculus",
+            "room" : "Lab",
+            "teacher" : "John",
+            "time" : "1:10",
+            "css" : "cal"
+        },
+        {
+            "class" : "Chinese",
+            "room" : "3A",
+            "teacher" : "Joy",
+            "time" : "1:55",
+            "css" : "chi"
+        },
+        {
+            "class" : "Physics",
+            "room" : "1A",
+            "teacher" : "John",
+            "time" : "2:40",
+            "css" : "phy"
+        }
+    ]
+}
